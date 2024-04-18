91db452975766999b1831da905f851b622bd610a
Subashh-Chaudhary <chysubash15963@gmail.com>
2024-04-18 10:11:50 +0545

    popular product section completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const Button = ({label, iconURL}) => {
   return (
     <button
-    className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
-          {label}
+    className='flex justify-center items-center gap-2 px-7 py-4 border bg-coral-red rounded-md'>
+      <p
+      className='font-montserrat text-lg leading-none text-white border-coral-red font-bold'>{label}</p>
+          
           <img src={iconURL} 
           alt="arrowRightIcon"
           className='ml-2 rounded-full w-5 h-5' />
