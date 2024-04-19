2904b7456928af915633ac15a54164f88aa82034
Subashh-Chaudhary <chysubash15963@gmail.com>
2024-04-19 15:47:57 +0545

    subscribe section completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Button = ({label, iconURL, spOffer}) => {
+const Button = ({label, iconURL, spOffer, fullWidth}) => {
   return (
     <button
     className={`flex justify-center items-center gap-2 px-7 py-4 border rounded-md
     ${spOffer ? 'bg-white border-slate-gray text-slate-gray' : ' text-white bg-coral-red '}
+    ${fullWidth && 'w-full'}
     `}>
       <p
       className='font-montserrat text-lg leading-none border-coral-red font-bold'>{label}</p>
