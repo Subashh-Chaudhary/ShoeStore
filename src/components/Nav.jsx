0f1f93fd1033f0bbde7409716d37b0c7379a5409
Subashh-Chaudhary <chysubash15963@gmail.com>
2024-04-20 22:11:37 +0545

    login and sign up added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,21 +10,40 @@ const Nav = () => {
         <a href="/">
           <img src={headerLogo} alt="logo" width={130} height={29} />
         </a>
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+        <ul className="flex justify-center items-center gap-7 xl:gap-16 max-lg:hidden">
           {navLinks.map((items) => (
             <li key={items.label}>
               <a
                 href={items.href}
-                className="font-montserrat leading-normal text-lg text-slate-gray"
+                className="font-montserrat font-semibold leading-normal text-lg hover:text-slate-gray text-gray-750"
               >
                 {items.label}
               </a>
             </li>
           ))}
         </ul>
+        <div
+        className="flex gap-10 max-md:gap-4 max-sm:gap-2">
+          <div
+        className="flex  justify-center items-center gap-1 max-sm:gap-0">
+          <a href="#"
+          className="font-montserrat font-semibold leading-normal text-lg text-gray-750 bg-transparent py-1 px-4 rounded-md hover:bg-slate-500  hover:text-white hover:rounded-md max-sm:text-[10px] max-sm:px-2">
+            Login
+          </a>
+          <p
+          >
+            |
+          </p>
+          <a href="#"
+          className="font-montserrat font-semibold leading-normal text-lg text-gray-750 bg-transparent py-1 px-4 rounded-md hover:bg-slate-500 hover:text-white hover:rounded-md max-sm:text-[10px] max-sm:px-2">
+            Sign up
+          </a>
+        </div>
         <div className="hidden max-lg:block">
           <img src={hamburger} alt="hamburger" width={25} height={25} />
+        </div>    
         </div>
+        
       </nav>
     </header>
   );
